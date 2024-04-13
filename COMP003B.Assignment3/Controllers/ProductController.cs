bd91cbe65021873166cf71044bdb71f498549c83
magalyrodriguez1 <142467680+magalyrodriguez1@users.noreply.github.com>
2024-04-12 20:42:58 -0700

    Add project files.

---
+++ COMP003B.Assignment3/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment3.Controllers
+{
+    public class ProductController : Controller
+    {
+
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ COMP003B.Assignment3/Models/product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment3.Models
+{
+    public class product
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        [Display(Name ="ID")]
+        public string Name { get; set; }
+        [Required]
+        [Range(0,100)]
+        public int Price { get; set; }
+    }
+}
